aded9208ab66fbcf4f4823a3859845969f1bd18b
v0sska <speedatom80k@gmail.com>
2024-02-10 01:54:26 +0200

    Add comments to change source elements

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ var gifContainer = document.getElementById("gif-container");
 
 function clickYesButton(){
 
-    image.src = "src/nya.jpg";
+    image.src = "src/nya.jpg"; //variable to change image
 
     let buttons = document.querySelectorAll(".button");
     buttons.forEach(button => button.style.display = "none");
 
-    h1.textContent = "УРАУРААРУАРУУРА";
-    h3.textContent = "люблю тебе настюшкінс:3";
+    h1.textContent = "УРАУРААРУАРУУРА"; //variable to change h1 text
+    h3.textContent = "люблю тебе настюшкінс:3"; //variable to change h3 text
 
     let audio = document.querySelector("#yesSound");
     audio.play();
@@ -26,15 +26,15 @@ function clickNoButton(){
 
     let noButton = document.querySelector("#no");
 
-    image.src = "src/wha.jpeg";
+    image.src = "src/wha.jpeg"; //variable to change image
 
     h1.textContent = "АХАХАХАХАХАХ,НАЇВНА))))";
 
     h3.textContent = "Вибач,але в тебе немає вибору:3";
 
     noButton.style.position = "absolute";
-    noButton.style.left = Math.random() * (window.innerWidth - noButton.offsetWidth) + "px";
-    noButton.style.top = Math.random() * (window.innerHeight - noButton.offsetHeight) + "px";
+    noButton.style.left = Math.random() * (window.innerWidth - noButton.offsetWidth) + "px"; //Generated random position of button to coordinates x
+    noButton.style.top = Math.random() * (window.innerHeight - noButton.offsetHeight) + "px";//Generated random position of button to coordinates y
 
     let sound = document.querySelector("#noSound");
     sound.play();
